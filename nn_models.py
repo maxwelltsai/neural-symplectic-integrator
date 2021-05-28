d0799606200f3d7e4cfe1271526a4ecb380be99d
Damian <damian@surfsara.nl>
2021-05-28 13:22:53 +0200

    new sparse

--- nn_models.py
+++ nn_models.py
@@ -36,28 +36,34 @@ class MLP(torch.nn.Module):
   '''Just a salt-of-the-earth MLP'''
   def __init__(self, input_dim, hidden_dim, output_dim, nonlinearity='tanh'):
     super(MLP, self).__init__()
-    self.linear1 = torch.nn.Linear(input_dim, hidden_dim*10)
-    self.linear2 = torch.nn.Linear(hidden_dim*10, hidden_dim*10)
-    self.linear3 = torch.nn.Linear(hidden_dim*10, output_dim, bias=None)
+    self.linear1 = torch.nn.Linear(input_dim, hidden_dim)
+    self.linear2 = torch.nn.Linear(hidden_dim, hidden_dim)
+    self.linear3 = torch.nn.Linear(hidden_dim, hidden_dim)
+    self.linear4 = torch.nn.Linear(hidden_dim, hidden_dim)
+    self.linear8 = torch.nn.Linear(hidden_dim, output_dim, bias=None)
 
-    for l in [self.linear1, self.linear2, self.linear3]:
+    for l in [self.linear1, self.linear2, self.linear3, self.linear4, self.linear8]:
       torch.nn.init.orthogonal_(l.weight) # use a principled initialization
-      
+
     if not os.path.isfile('mask_file'):
-      mask = torch.tensor(sparse.random(hidden_dim*10, hidden_dim*10, density=0.2).toarray())
+      mask = torch.tensor(sparse.random(hidden_dim, hidden_dim, density=0.1).toarray())
       torch.save(mask, "mask_file")
     else:
       mask = torch.load("mask_file")
 
     with torch.no_grad():
       self.linear2.weight.mul_(mask)
+      self.linear3.weight.mul_(mask)
+      self.linear4.weight.mul_(mask)
 
     self.nonlinearity = choose_nonlinearity(nonlinearity)
 
   def forward(self, x, separate_fields=False):
     h = self.nonlinearity( self.linear1(x) )
     h = self.nonlinearity( self.linear2(h) )
-    return self.linear3(h)
+    h = self.nonlinearity( self.linear3(h) )
+    h = self.nonlinearity( self.linear4(h) )
+    return self.linear8(h)
 
 class MLP_vanilla(torch.nn.Module):
   '''Just a salt-of-the-earth MLP'''
